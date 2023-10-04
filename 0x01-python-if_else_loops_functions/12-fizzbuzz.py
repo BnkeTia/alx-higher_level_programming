cd6a91b8e678b2ca69f39c76b4dfdba57c39dd4d
BnKeTia <bernardnketia51@gmail.com>
2023-10-04 02:42:00 +0000

    sorted out remaining files

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
+    for digit in range(1, 101):
+        if digit % 5 == 0 and digit % 3 == 0:
             print("FizzBuzz", end=' ')
-        elif i % 3 == 0:
+        elif digit % 3 == 0:
             print("Fizz", end=' ')
-        elif i % 5 == 0:
+        elif digit % 5 == 0:
             print("Buzz", end=' ')
         else:
-            print(i, end=' ')
+            print("{:d}".format(digit), end=' ')
