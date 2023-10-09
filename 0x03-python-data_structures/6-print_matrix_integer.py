3d5204a18ac5ce0421a5918388637acd46de436a
BnKeTia <bernardnketia51@gmail.com>
2023-10-09 11:20:38 +0000

    task6 control code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    # Iterate through each row in the matrix
+
     for row in matrix:
-        # Iterate through each column in the current row
-        for column in range(len(row)):
-            # Print the integer value from the matrix with formatting
-            print("{:d}".format(row[column]), end="")
-            # Add a space if it's not the last column in the row
-            if column < len(row) - 1:
+        # Iterarte through each integer in the current row
+        for integer in range(len(row)):
+            print("{:d}" .format(row[integer]), end=" ")
+            # add a space if next integer is not the last
+            if integer < len(row) - 1:
                 print(" ", end="")
-        # Print a newline character to move to the next row
-        print("")
+
+        print("")  # print a new line
