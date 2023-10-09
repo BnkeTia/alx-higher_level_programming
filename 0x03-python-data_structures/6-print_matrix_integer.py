5196d4e34af745bed07cad64821b7b40ed9d4cf8
BnKeTia <bernardnketia51@gmail.com>
2023-10-09 11:27:56 +0000

    excluded space character

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         # Iterarte through each column in the current row
         for column in range(len(row)):
-            print("{:d}" .format(row[column]), end=" ")
+            print("{:d}" .format(row[column]), end="")
 
             # add a space if next column is not the last
             if column < len(row) - 1:
