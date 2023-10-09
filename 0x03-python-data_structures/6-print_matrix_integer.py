dd86382fae970642a5b4abcdb4e42e977ced8b66
BnKeTia <bernardnketia51@gmail.com>
2023-10-09 11:29:24 +0000

    fixed null printing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,11 +4,11 @@ def print_matrix_integer(matrix=[[]]):
 
     for row in matrix:
         # Iterarte through each column in the current row
-        for column in range(len(row)):
-            print("{:d}" .format(row[column]), end="")
+        for integer in range(len(row)):
+            print("{:d}" .format(row[integer]), end="")
 
-            # add a space if next column is not the last
-            if column < len(row) - 1:
+            # add a space if next integer is not the last
+            if integer < len(row) - 1:
                 print(" ", end="")
 
         print("")  # print a new line
