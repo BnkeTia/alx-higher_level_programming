baddd433467a5d22b52316d7a72900f11f3b16a2
BnKeTia <bernardnketia51@gmail.com>
2023-10-09 13:10:55 +0000

    only by 2 and delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -7,6 +7,6 @@ def delete_at(my_list=[], idx=0):
         return my_list
 
     # Create new list with list slicing
-    new_list = my_list[:idx] + my_list[idx+1:]
+    my_list[:] = my_list[:idx] + my_list[idx+1:]
 
-    return new_list
+    return my_list
