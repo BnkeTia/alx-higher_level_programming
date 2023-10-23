67669ae6c693533963287a8f6806333ea728850f
BnKeTia <bernardnketia51@gmail.com>
2023-10-23 16:39:35 +0000

    recheck 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,40 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    printed_integers = 0  # set number of inegers printed to 0
-
+    count = 0  # Initialize a counter for the number of integers printed
+    for item in my_list:
+        if count >= x:
+            break  # Exit the loop once x integers are printed
         try:
-            for element in my_list:
-                if isinstance(element, int):
-                        print("{:d}" .format(element), end="")
-                        printed_integers += 1
-                        if printed_integers >= x:
-                            break
+            print("{:d}".format(item), end="")
+            count += 1  # Increment the counter
+        except (ValueError, TypeError):
+            pass  # Skip non-integer values
+    print()  # Print a new line after all integers
+    return count
 
+
+def safe_print_list_integers(my_list=[], x=0):
+    total_count = 0
+    for i in range(x):
+        try:
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                total_count += 1
+            else:
+                continue
         except IndexError:
-            pass
-    print()  # Add a new line
-    return printed_integers
+            break
+    print()
+    return total_count
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return count
