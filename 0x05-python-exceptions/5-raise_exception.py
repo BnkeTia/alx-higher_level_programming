cae8a325d5745d4933e48d32cae685325d5f9d11
BnKeTia <bernardnketia51@gmail.com>
2023-10-23 19:39:21 +0000

    raisng all

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -4,7 +4,4 @@ def raise_exception():
     raise TypeError("This is a custom type exception")
 
 
-try:
-    raise_exception()
-except TypeError as e:
-    print(e)
+# try: raise_exception()except TypeError as e:print(e)
