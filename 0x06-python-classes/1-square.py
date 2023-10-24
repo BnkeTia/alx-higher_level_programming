5874eb40dd34bfa4799a7d77f4c109b9bce0beae
BnKeTia <bernardnketia51@gmail.com>
2023-10-24 16:36:41 +0000

    init square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    A class square that defines a square
+
+    """
+    def __init__(self, size):
+
+        """
+        instantiation with size
+
+        """
+        self._size = size
