d218ba49c6c2ea87a2750a54b3a78f0cc687def6
BnKeTia <bernardnketia51@gmail.com>
2023-10-24 16:12:16 +0000

    adding docstrings

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
+"""This is an empty class square that defines a square"""
 class square:
     pass
+"""creating an object instance to confirm variable type"""
 s = square()
 print(s)
