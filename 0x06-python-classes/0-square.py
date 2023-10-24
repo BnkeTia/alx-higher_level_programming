80cc38634a4d1bb07913f2c46fc39685ea5cdb7a
BnKeTia <bernardnketia51@gmail.com>
2023-10-24 16:25:49 +0000

    chaley we dey

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""
+This is a module for defining a Square class.
+"""
+
 
 class Square:
     """
-    This is an empty class square that defines a Square
-
+    An empty class that defines a square.
     """
-
     pass
