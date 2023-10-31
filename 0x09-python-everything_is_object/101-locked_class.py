67ad4ab52ca422ad5b0fa9e0ee4f9a7522e6ffa9
BnKeTia <bernardnketia51@gmail.com>
2023-10-31 22:56:59 +0000

    LockedGlass

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+
+"""
+class LockedClass with no object or attribute
+"""
+
+
 class LockedClass:
+    """
+    fuction prevents the user from dynamically creating instances
+    """
+
     def __setattr__(self, name, value):
         if name != "first_name":
             raise AttributeError(
