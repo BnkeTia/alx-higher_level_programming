eecd7bd8085fbce36b539dc0a5220bfd8b6bed58
BnKeTia <bernardnketia51@gmail.com>
2023-10-31 12:44:19 +0000

    sqaure of 5

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -7,26 +7,20 @@ This module is composed by a function that prints a square with the character #
 
 
 def print_square(size):
-    """ Function that prints a square with the character #
+    """
+    Function that prints a square with the character #
 
-    Args:
-        size: size of the square printed
-
-    Returns:
-        No return
-
-    Raises:
-        TypeError: If size is not an integer number
-
+    Prints the size of the square.
 
     """
     # check if size is an integer
-    if not isinstance(size, int):
+    if not isinstance(size, int) or (isinstance(size, float) and
+                                     size.is_integer() is False):
         raise TypeError("size must be an integer")
     # check if size is a non-integer negative
     if size < 0:
         raise ValueError("size must be >= 0")
     # print the square with the character #
 
-    for count in range(size):
+    for i in range(size):
         print("#" * size)
