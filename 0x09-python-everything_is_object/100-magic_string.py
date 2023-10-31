48e042b3c690a9b54cd7af2350864416d9870a29
BnKeTia <bernardnketia51@gmail.com>
2023-10-31 20:13:36 +0000

    pythonic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,2 +1,4 @@
-def magic_string(n=1, s=""):
-    return "BestSchool" + s if n == 1 else magic_string(n -1, "BestSchool" + s)
+def magic_string():
+    return ', '.join(['BestSchool'] * magic_string.counter)
+magic_string.counter = 0
+magic_string.counter += 1
