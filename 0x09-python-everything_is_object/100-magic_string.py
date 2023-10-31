6f5a3f2b7985e8106563be50d486274775c8bebc
BnKeTia <bernardnketia51@gmail.com>
2023-10-31 21:17:49 +0000

    4 line limit

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
-def magic_string(n=1, s=""):
-    return "BestSchool" + s if n == 1 else magic_string(n - 1,
-                                                        "BestSchool" + s)
+#!/usr/bin/pythons3
+def magic_string(n=1):
+    return "BestSchool" + (", " + magic_string(n - 1) if n > 1 else "")
