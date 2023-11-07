b8230d42551dd14562391d908bad4717daaf872a
BnKeTia <bernardnketia51@gmail.com>
2023-11-07 16:45:04 +0000

    rearrange docstrings

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,10 +4,14 @@ A module json that contain a function
 
 """
 import json
-"""
-A function that converts an object to a json string
 
-"""
+
 
 def to_json_string(my_obj):
+    """
+    A function that converts an object to a json string
+
+
+    """
+
     return json.dumps(my_obj)
