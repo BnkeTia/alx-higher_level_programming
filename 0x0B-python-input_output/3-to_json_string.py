d8951e461a6d49785ef7c81b57c1420934073721
BnKeTia <bernardnketia51@gmail.com>
2023-11-07 16:42:26 +0000

    converts to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A module json that contain a function
+
+"""
+import json
+"""
+A function that converts an object to a json string
+
+"""
+
+def to_json_string(my_obj):
+    return json.dumps(my_obj)
