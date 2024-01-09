1a5b3429152d98eb53bc4888e4d292c38f40b705
BnKeTia <bernardnketia51@gmail.com>
2024-01-09 11:37:22 +0000

    edit arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-/* A script that prints a message depending of the number of arguments passed:
+/* A script that prints a message depending on the number of arguments passed:
  * If no arguments are passed to the script, print “No argument”
  * If only one argument is passed to the script, print “Argument found”
- * Otherwise, print “Arguments found”
+ * Otherwise, print “Argument found”
  */
 
 const args = process.argv.length;
